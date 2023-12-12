56573eb463a658920f59129fd795fcaa1213224b
nitehogg <keith.b.elliott@gmail.com>
2023-12-11 23:17:52 -0500

    saving progress

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,25 +1,26 @@
+-- figure out some way to extend default options in lua
 local default_opts = { noremap = true, silent = true }
 
--- figure out some way to extend default options in lua
-vim.keymap.set("n", "<leader>l", ":set relativenumber<CR>", {
-  noramp = true,
-  silent = true,
-  desc = "Use Relative Number"
-})
+-- vim.keymap.set("n", "<leader>l", ":set relativenumber<CR>", {
+--   noramp = true,
+--   silent = true,
+--   desc = "Use Relative Number"
+-- })
 -- vim.keymap.set("n", "<leader>lx", ":set norelativenumber<CR>", {
 --   noramp = true,
 --   silent = true,
 --   desc = "Disable Relative Number"
 -- })
 
--- vim.keymap.set("n", "<leader>l",
---   function()
---    return vim.o.relativenumber and ":set norelativenumber<CR>" or ":set relativenumber<CR>"
---   end, {
---   noramp = true,
---   silent = true,
---   desc = "Toggle Relative Number"
--- })
+vim.keymap.set("n", "<leader>l",
+  function()
+   return vim.o.relativenumber and "<cmd>set norelativenumber<CR>" or "<cmd>set relativenumber<CR>"
+  end, {
+    remap = false,
+    silent = true,
+    desc = "Toggle Relative Number"
+  }
+)
 
 
 
