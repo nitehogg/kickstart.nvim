9f4eef8bde3979e07faa9ab62208c139c00cd670
Keith Elliott <keith.elliott@beckshybrids.com>
2024-07-11 17:09:39 -0400

    adds omnisharp

--- lua/custom/configs/loaders/nvim_lspconfig.lua
+++ lua/custom/configs/loaders/nvim_lspconfig.lua
@@ -2,9 +2,10 @@ local nvim_lsp = require('lspconfig')
 
 local module = {}
 
-function module.setup(on_attach)
+function module.setup(on_attach, capabilites)
   nvim_lsp.denols.setup {
     on_attach = on_attach,
+    capabilites = capabilites,
     root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
   }
 
@@ -14,8 +15,14 @@ function module.setup(on_attach)
       client.server_capabilities.documentRangeFormattingProvider = false;
       on_attach(client, bufnr);
     end,
+    capabilites = capabilites,
     root_dir = nvim_lsp.util.root_pattern("package.json"),
     single_file_support = false,
+  }
+
+  nvim_lsp.omnisharp.setup {
+    on_attach = on_attach,
+    capabilites = capabilites,
   }
 end
 
